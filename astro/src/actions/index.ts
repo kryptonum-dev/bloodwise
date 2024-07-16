4b17a2c5e94626fcf0c0348304bedf52ea38a727
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-07-16 13:01:05 +0200

    create sendContactForm as function

--- astro/src/actions/index.ts
+++ astro/src/actions/index.ts
@@ -1,4 +1,5 @@
 import { defineAction, z } from "astro:actions";
+import { sendContactEmail } from "./sendContactForm";
 
 export const server = {
   contact: defineAction({
@@ -9,13 +10,8 @@ export const server = {
       message: z.string().min(3, { message: 'Wiadomość jest wymagana' }),
       legal: z.preprocess(value => value === 'on', z.boolean()).refine(value => value, { message: "You must agree to the terms and conditions", }),
     }),
-    handler: async (data) => {
-      const response = await fetch(`http://localhost:4321/api/contact-form`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-      return await response.json() as { success: boolean };
+    handler: async ({ name, email, message }) => {
+      return await sendContactEmail({ name, email, message });
     },
   }),
 };
---
+++ astro/src/actions/sendContactForm.ts
@@ -0,0 +1,45 @@
+
+import { removeHtmlTags } from '@/utils/remove-html-tags';
+
+type Props = {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export async function sendContactEmail({ name, email, message }: Props) {
+  const body = [
+    `<p>Imię: <b>${name}</b></p>`,
+    `<p>Adres e-mail: <b>${email}</b></p>`,
+    `<p>${message.trim()}</p>`,
+    '<br />',
+    '<br />',
+    '<p><em>Wyrażono zgodę na politykę prywatności</em></p>'
+  ].join('');
+
+  try {
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${import.meta.env.RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: `${name} ze strony Bloodwise <formularz@bloodwise.com.pl>`,
+        to: 'contact@bloodwise.com.pl',
+        reply_to: `${name} <${email}>`,
+        subject: `${name} przesyła wiadomość przez formularz kontaktowy`,
+        html: body,
+        text: removeHtmlTags(body),
+      }),
+    });
+
+    if (res.ok) {
+      return { success: true, message: "Successfully sent" };
+    } else {
+      return { success: false, message: "Failed to send" };
+    }
+  } catch {
+    return { success: false, message: "An error occurred while sending the email" };
+  }
+}

5115a81125aeed042865d5524f05a5e71ac6cca4
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-07-16 17:19:31 +0200

    Change validation to contact actions

--- astro/src/actions/index.ts
+++ astro/src/actions/index.ts
@@ -5,9 +5,9 @@ export const server = {
   contact: defineAction({
     accept: "form",
     input: z.object({
-      name: z.string().min(2, { message: 'Imię jest wymagane' }),
+      name: z.string().min(1, { message: 'Imię jest wymagane' }),
       email: z.string().email({ message: 'Podaj prawidłowy adres email' }),
-      message: z.string().min(3, { message: 'Wiadomość jest wymagana' }),
+      message: z.string().min(1, { message: 'Wiadomość jest wymagana' }),
       legal: z.preprocess(value => value === 'on', z.boolean()).refine(value => value, { message: "You must agree to the terms and conditions", }),
     }),
     handler: async ({ name, email, message }) => {

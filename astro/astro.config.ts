b4a580ddae21aaf7955ccabfb3d153cb660a62b4
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-07-18 14:43:26 +0200

    Add imagesConfig for vercel adapter

--- astro/astro.config.ts
+++ astro/astro.config.ts
@@ -26,5 +26,15 @@ export default defineConfig({
   output: isPreviewDeployment ? "server" : 'hybrid',
   adapter: vercel({
     imageService: true,
+    imagesConfig: {
+      sizes: [48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      domains: [],
+      formats: ['image/avif', 'image/webp'],
+      dangerouslyAllowSVG: true,
+      remotePatterns: [{
+        protocol: "https",
+        hostname: "cdn.sanity.io"
+      }],
+    }
   })
 });
